1761bca065a72702f9ca7f02726901254d1f3046
Abhishek Jain <61509797+abhishekjainin88@users.noreply.github.com>
2024-06-26 08:25:02 +0530

    Update text_splitter.py

---
+++ ChatWithDoc/DocumentCommunicator/Components/text_splitter.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from DocumentCommunicator.Logger import logging
+from DocumentCommunicator.Exception import CustomException
+
+from DocumentCommunicator.Entity.config_entity import SplitterConfig
+
+
+class Splitter:
+    def __init__(self, splitter_config: SplitterConfig):
+        self.splitter_config = splitter_config
+    
+    def get_splitter_object(self):
+        try:
+            logging.info("Entered the get_splitter_object method of Embedding class")
+            if self.splitter_config.TEXT_SPLITTER == "RecursiveCharacterTextSplitter":
+                
+                open_ai_api_key = self.aapi_loader_config.OPENAI_API_KEY
+                splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=64)
+                return splitter
+            logging.info("Exited the get_splitter_object method of Data ingestion class")
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    
