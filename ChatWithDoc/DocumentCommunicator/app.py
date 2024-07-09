d840142ac06dcf9a3226eefa76fb23c8263bbbd6
abhishekjainin88 <abhishekjainin88@gmail.com>
2024-07-09 17:13:42 +0530

    Added missing code

--- ChatWithDoc/DocumentCommunicator/app.py
+++ ChatWithDoc/DocumentCommunicator/app.py
@@ -10,16 +10,19 @@ vector_store=Service.get_vectordb_instance()
 
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template('chat.html')    
+
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    question = request.form["msg"]
- 
+    question = request.form["msg"]    
     result=Service.Rag_Chain_invoke(question,vector_store)
     #result=chain.invoke(input)
     print("Response : ", result)
-    return str(result)
+    return str(result)   
+
+    #Service.evaluate_rag(vector_store)
+    #return "Success"
 
 if __name__ == '__main__':
     app.run(debug= True)
